a3871293481e24fac3f615b3f947d1bd16d7eba0
Virajj28 <viraj28j@gmail.com>
2022-02-08 13:33:24 +0530

    strngs & char access

--- index.js
+++ index.js
@@ -2,8 +2,34 @@
 //     console.log('Hello World!'+i)
 // }
 
-function sum (a, b){
-    return a*b;
+const { strictEqual } = require("assert")
+
+// function sum (a, b){
+//     return a*b;
+// }
+
+// console.log(sum(6,3))
+
+// function getCharCount (str){
+//     return str.length;
+// }
+
+// console.log(getCharCount('Hello World'))
+
+// function toUpperCase (str){
+//     return str.toUpperCase();
+// }
+
+// console.log(toUpperCase('viraj jadhav'))
+
+// function getFirstCharacter(name){
+//     return name[0]
+// }
+
+// console.log(getFirstCharacter('viraj'))
+
+function getLastCharacter(name){
+    return name[name.length-1]
 }
 
-console.log(sum(6,3))
+console.log(getLastCharacter('viraj'))
