c9c22bb34f5b6bc7db2106dd4d664ca2ee1cf6b1
Virajj28 <viraj28j@gmail.com>
2022-02-08 14:06:15 +0530

    substrng, plus oprtr,templte strngs

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 //     console.log('Hello World!'+i)
 // }
 
-const { strictEqual } = require("assert")
 
 // function sum (a, b){
 //     return a*b;
@@ -22,14 +21,73 @@ const { strictEqual } = require("assert")
 
 // console.log(toUpperCase('viraj jadhav'))
 
+
 // function getFirstCharacter(name){
 //     return name[0]
 // }
 
 // console.log(getFirstCharacter('viraj'))
 
-function getLastCharacter(name){
-    return name[name.length-1]
+// function getLastCharacter(name){
+//     return name[name.length-1]
+// }
+
+// console.log(getLastCharacter('viraj'))
+
+
+// function skipFirstCharacter(text){
+//     // return text.substring(1)
+//     return text.slice(1)
+// }
+// console.log(skipFirstCharacter('Xcode'))
+// console.log(skipFirstCharacter('Bello'))
+
+// function getDescription (text){
+//     return text.slice(0,10)
+// }
+
+
+// function concateInitials (firstNameIntial, lastNameInitial){
+//     return firstNameIntial + ' ' + lastNameInitial
+// }
+
+// console.log(concateInitials('Viraj','Jadhav'))
+
+// function getDescription (text) {
+//         return text.slice(0,10) + '...'
+// }
+
+// console.log(getDescription('BellaCiaBellaCiao'))
+
+
+// Template Strings
+// STRING INTERPOLATION ✍️
+// function sayHello (name){
+//     return 'Hello' + ' ' + name; 
+// }
+
+// console.log(sayHello("Alex")); // "Hello Alex"
+// console.log(sayHello("Sam")); // "Hello Sam"
+
+// function getFullName (fname, lname) {
+//     return `${fname} ${lname}`
+// }
+
+// console.log(getFullName('Viraj', 'Jadhav'))
+
+// function getMultilineString(){
+//     return  `I am learning JavaScript  
+//     and I found it to be  
+//     quite fun!`
+// }
+
+// console.log(getMultilineString());
+
+function capitalize(word) {
+    return word[0].toUpperCase() + word.slice(1).toLowerCase();
 }
 
-console.log(getLastCharacter('viraj'))
+// Sample usage - do not modify
+console.log(capitalize("sam")); // "Sam"
+console.log(capitalize("ALEX")); // "Alex"
+console.log(capitalize("chARLie")); // "Charlie"
