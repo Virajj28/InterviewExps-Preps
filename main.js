420e12741f2b43e9d205552a757947446507b67d
Virajj28 <viraj28j@gmail.com>
2022-02-18 12:59:22 +0530

    digimaze opprt

---
+++ main.js
@@ -0,0 +1,21 @@
+// console.log('Hello JS !');
+
+function calc (num1,num2,callbackFunction){
+    callbackFunction(num1,num2);
+}
+
+// function calcSum(num1,num2){
+//     calcSum(num1*num2);
+// }
+
+// console.log(calc(2,2,calcSum))
+
+// useEffect ( () => {
+//     fetch('https://')
+//     .then(res => res.json)
+//     .then(data => {
+//         console.log(data);
+//     })
+// }, [])
+
+Animations in JS, Keyframes 
