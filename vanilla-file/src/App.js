5a20964745414d6e3ab3d4bd5d6968956d8977dc
Virajj28 <viraj28j@gmail.com>
2022-04-12 14:24:12 +0530

    using 'rfce' fst_code_extnsn

--- vanilla-file/src/App.js
+++ vanilla-file/src/App.js
@@ -1,11 +1,14 @@
 import './App.css'
 import Counter from './components/Counter'
+import Searchbar from './components/Searchbar';
 
 function App() {
     return (
-        <div className='text-lg text-center italic bg-lime-600'>
-            Hello World ! ==
+        <div className='grid justify-center align-center'>
+        <div className='text-lg italic bg-lime-600'>
+            Hello World ! ==</div>
             <Counter />
+            <Searchbar />
         </div>
     );
 }
