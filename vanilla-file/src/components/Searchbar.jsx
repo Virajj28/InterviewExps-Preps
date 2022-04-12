01dc08a6dfab23768bf00148a5b2fec962b1b499
Virajj28 <viraj28j@gmail.com>
2022-04-12 16:16:28 +0530

    created components

--- vanilla-file/src/components/Searchbar.jsx
+++ vanilla-file/src/components/Searchbar.jsx
@@ -1,7 +1,36 @@
 import { useState,useEffect } from "react";
 
+const DropDown = () => {
+  return (
+    <div>
+      DropDown
+    </div>
+  )
+}
+
+const TextArea = () => {
+  return(
+    <>
+      <input className="w-96 h-8 focus:border-2 focus:border-yellow-800" placeholder="Enter to search"></input>
+    </>
+  )
+}
+
+function SearchIcon() {
+  return (
+    <div className='bg-yellow-500 w-9'>
+      SearchIcon
+    </div>
+  )
+}
+
+
 export default function Searchbar() {
   return (
-    <div>Searchbar</div>
+    <div className="flex align-center justify-center">
+      <DropDown />
+      <TextArea />
+      <SearchIcon />
+    </div>
   )
 }
