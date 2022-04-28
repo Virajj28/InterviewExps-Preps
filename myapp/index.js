72db889c73cae5b80ef3b344f107ce5fdf7b50f4
Virajj28 <viraj28j@gmail.com>
2022-04-28 18:50:38 +0530

    fixed addn w/ parseInt

--- myapp/index.js
+++ myapp/index.js
@@ -4,13 +4,13 @@ const port = 3000
 
 app.get('/', (req, res) => {
     console.log(req.query)
-  res.send(req.query.number1+req.query.number2)
+  // Solution by Tech lead in live sorted with parseInt
+  // res.send(req.query.number1+req.query.number2)
+  res.send(parseInt(req.query.number1)+parseInt(req.query.number2))
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-// ## Query number1 and Query number2 output is displayed in strings.
-// eg. 1+1 = 2 ...output is displayed as 11
 // http://localhost:3000/?number1=1&number2=2
